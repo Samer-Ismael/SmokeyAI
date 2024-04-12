75aefcfa670d9fa770801794fcc87f80c93b917e
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-04-13 01:03:06 +0200

    Saving

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 history = []
 
 @app.route('/generate', methods=['POST'])
@@ -12,9 +12,10 @@ def generate_response():
     url = 'http://localhost:11434/api/generate'
     data = request.json
 
-    # Set the model name
+    # Set the model name to "Smokey"
     data['model'] = 'Smokey'
-    # Disable streaming
+
+    # Set streaming to false
     data['stream'] = False
 
     # Add conversation history to the prompt
@@ -31,13 +32,14 @@ def generate_response():
     response_data = response.json()
     generated_response = response_data.get('response', '')
 
+    # Print the generated response
+    print("Generated response:", generated_response)
+
     # Append the generated response to the history
     history.append(generated_response)
 
     # Return the generated response along with the history
     return jsonify({'generated_response': generated_response, 'history': list(history)})
 
-
-
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
